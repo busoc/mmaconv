f8dc7a66cdd86341e68026d22c6b5cb76acd3e51
midbel <brun.nicolas21@gmail.com>
2021-04-04 10:13:57 +0200

    exit on error

--- scripts/mmabatch.sh
+++ scripts/mmabatch.sh
@@ -8,6 +8,9 @@ IN=$4
 while true; do
   FILE="$OUT/$(date -d "$START" +%Y/%j).csv.gz"
   mmaconv -z -r -w $FILE "$IN/$(date -d $START +%Y/%j)"
+  if [[ $? -ne 0 ]]; then
+    exit 1
+  fi
 
   if [[ $START == $END ]]; then
     break;
