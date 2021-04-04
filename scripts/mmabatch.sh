e6bc95cf521c9b44fa945b34ec37a13da8b74213
midbel <brun.nicolas21@gmail.com>
2021-04-04 10:11:27 +0200

    small scripts to process mma file in a row

---
+++ scripts/mmabatch.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+START=$(date -d "$1" +%Y-%m-%d)
+END=$(date -d "$2" +%Y-%m-%d)
+OUT=$3
+IN=$4
+
+while true; do
+  FILE="$OUT/$(date -d "$START" +%Y/%j).csv.gz"
+  mmaconv -z -r -w $FILE "$IN/$(date -d $START +%Y/%j)"
+
+  if [[ $START == $END ]]; then
+    break;
+  fi
+  START=$(date -d "$START + 1 days" +%Y-%m-%d)
+done
